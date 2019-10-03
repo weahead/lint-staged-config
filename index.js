3cd0130fb25f58f12daed7760e01175b3106302a
Christian Andolf <christian@weahead.se>
2019-10-03 10:24:32 +0200

    feat: removed eslint fix for prettier

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '*.{js,jsx,json,ts,tsx}': ['eslint --fix', 'git add'],
-  '*.json': ['prettier --write', 'git add'],
-  '*.html': ['prettier --write', 'git add'],
+  '*.{js,jsx,json,ts,tsx,html}': ['prettier --write', 'git add'],
   '*.md': ['prettier --write', 'markdownlint', 'git add'],
 };
