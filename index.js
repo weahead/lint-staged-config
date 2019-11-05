5f75a51285adf960009d4bdd57c4c55bff537fab
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-11-05 12:50:28 +0100

    feat(json): don't run json files through eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,json,ts,tsx}': ['eslint --fix', 'git add'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'git add'],
   '*.json': ['prettier --write', 'git add'],
   '*.html': ['prettier --write', 'git add'],
   '*.md': ['prettier --write', 'markdownlint', 'git add'],
